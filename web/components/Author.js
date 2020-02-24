dfe73dd831d25c787379f06bdde2d64f9ce0b40b
rick <fang@rickdeMacBook-Pro.local>
2020-02-24 14:56:51 +0800

    add author

---
+++ web/components/Author.js
@@ -0,0 +1,23 @@
+
+import {Avatar,Divider} from 'antd'
+import './styles/author.less'
+
+const Author =()=>{
+
+    return (
+        <div className="author-div comm-box">
+            <div> <Avatar size={100} src="https://rickfang666.github.io/uploads/avatar.png"  /></div>
+            <div className="author-introduction">
+                职业写码，业余写字，just so so...
+                <Divider>社交账号</Divider>
+                <Avatar size={28} icon="github" className="account"  />
+                <Avatar size={28} icon="qq"  className="account" />
+                <Avatar size={28} icon="wechat"  className="account"  />
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Author
--- web/pages/detailed.js
+++ web/pages/detailed.js
@@ -2,26 +2,23 @@ import React from 'react'
 import Head from 'next/head'
 import {Row, Col} from 'antd'
 import Header from '../components/Header'
-import Author from '../components/Author'
-import Advert from '../components/Advert'
-
-const Home = () => (
+const Detailed = () => (
   <>
     <Head>
-      <title>Rick's Blog</title>
+      <title>Detailed</title>
     </Head>
-    <Header /> 
+    <Header />
     <Row className="comm-main" type="flex" justify="center">
       <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
         左侧
       </Col>
 
       <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-      <Author />
-      <Advert />
+        右侧
       </Col>
     </Row>
+
  </>
 )
 
-export default Home
+export default Detailed
