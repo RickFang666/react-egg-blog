5fc08aef8740f7da9289d66ce4aee5712358a022
rick <fang@rickdeMacBook-Pro.local>
2020-02-24 16:44:46 +0800

    add markdown

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,19 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less')
+const withLess = require("@zeit/next-less");
+const withCss = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
   require.extensions['.less'] = (file) => {}
 }
 
-module.exports = withLess({
+// module.exports = withLess({
+//   lessLoaderOptions: {
+//     javascriptEnabled: true,
+//   },
+// })
+module.exports = withPlugins([withLess,withCss],{
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
--- web/pages/detailed.js
+++ web/pages/detailed.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
 import {Row, Col , Icon ,Breadcrumb  } from 'antd'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
 
 import Header from '../components/Header'
 import Author from '../components/Author'
@@ -8,6 +11,41 @@ import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 
 import './styles/detail.less'
+
+let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
 
 const Detailed = () => {
   return (
@@ -39,7 +77,19 @@ const Detailed = () => {
                 </div>
 
                 <div className="detailed-content" >
-                  详细内容，下节课编写
+                  <ReactMarkdown 
+                    source={markdown} 
+                    escapeHtml={false}  
+                  />
+                </div>
+
+                <div className="detailed-nav comm-box">
+                  <div className="nav-title">文章目录</div>
+                  <MarkNav
+                    className="article-menu"
+                    source={markdown}
+                    ordered={false}
+                  />
                 </div>
 
              </div>
