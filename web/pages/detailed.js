019bc08a40597038210600ad947b2d300c53fc93
rick <fang@rickdeMacBook-Pro.local>
2020-04-14 01:35:42 +0800

    update detail article content

--- web/pages/detailed.js
+++ web/pages/detailed.js
@@ -5,6 +5,9 @@ import {Row, Col , Icon ,Breadcrumb  } from 'antd'
 import ReactMarkdown from 'react-markdown'
 import MarkNav from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
+import marked from 'marked'
+import hljs from "highlight.js";
+import 'highlight.js/styles/monokai-sublime.css';
 
 import Header from '../components/Header'
 import Author from '../components/Author'
@@ -13,42 +16,29 @@ import Footer from '../components/Footer'
 
 import './styles/detail.less'
 
-let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+let markdown='# P02:课程介绍和环境搭建\n' +   '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +   '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +    '**这是加粗的文字**\n\n' +   '*这是倾斜的文字*`\n\n' +   '***这是斜体加粗的文字***\n\n' +   '~~这是加删除线的文字~~ \n\n'+   '\`console.log(111)\` \n\n'+   '# p02:来个Hello World 初始Vue3.0\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n'+   '***\n\n\n' +   '# p03:Vue3.0基础知识讲解\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n\n'+   '# p04:Vue3.0基础知识讲解\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n\n'+   '#5 p05:Vue3.0基础知识讲解\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n\n'+   '# p06:Vue3.0基础知识讲解\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n\n'+   '# p07:Vue3.0基础知识讲解\n' +   '> aaaaaaaaa\n' +   '>> bbbbbbbbb\n' +   '>>> cccccccccc\n\n'+   '``` var a=11; ```'
 
-const Detailed = () => {
+  const renderer = new marked.Renderer();
+
+marked.setOptions({
+    renderer: renderer, 
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+    }
+  }); 
+
+// const html = marked(props.article_content) 
+// const html = marked(markdown) 
+
+const Detailed = (props) => {
+const html = marked(props.article_content) 
   return (
     <>
       <Head>
@@ -77,18 +67,21 @@ const Detailed = () => {
                   <span><Icon type="fire" /> 5498人</span>
                 </div>
 
-                <div className="detailed-content" >
+                <div className="detailed-content" dangerouslySetInnerHTML={{__html: html}}></div>
+                {/* <div className="detailed-content" >
                   <ReactMarkdown 
+                    // source={props.article_content} 
                     source={markdown} 
                     escapeHtml={false}  
                   />
-                </div>
+                </div> */}
 
                 <div className="detailed-nav comm-box">
                   <div className="nav-title">文章目录</div>
                   <MarkNav
                     className="article-menu"
-                    source={markdown}
+                    // source={props.article_content}
+                    source={markdown} 
                     ordered={false}
                   />
                 </div>
