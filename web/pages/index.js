d95d944aeb7dd9af0bb022283bf5444d62c5d1eb
rick <fang@rickdeMacBook-Pro.local>
2020-04-15 02:17:32 +0800

    update index layout

--- web/pages/index.js
+++ web/pages/index.js
@@ -42,7 +42,7 @@ const Home = (list) => {
       </Head>
       <Header />
       <Row className='comm-main' type='flex' justify='center'>
-        <div className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
+        <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
           <div>
             <List
               header={<div>最新日志</div>}
@@ -79,7 +79,7 @@ const Home = (list) => {
               )}
             />
           </div>
-        </div>
+        </Col>
 
         <Col className='comm-box' xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
