cd685a8c45582191baf17ff5f4d541b8752e03c8
rick <fang@rickdeMacBook-Pro.local>
2020-04-18 23:57:02 +0800

    add admin build

---
+++ admin/build/precache-manifest.3d41f6c4791588489eca00cec1d07e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e23a353ccd0807b9be5d6c829c75655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b26876bc0cebf7bc762",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "7dfa9a5a2904fa5620b5",
+    "url": "/static/css/main.544d3f5f.chunk.css"
+  },
+  {
+    "revision": "7b26876bc0cebf7bc762",
+    "url": "/static/js/2.eaae3d80.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.eaae3d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfa9a5a2904fa5620b5",
+    "url": "/static/js/main.bb1bfe44.chunk.js"
+  },
+  {
+    "revision": "960dd191cb48cb679f54",
+    "url": "/static/js/runtime-main.e6c4b380.js"
+  }
+]);
